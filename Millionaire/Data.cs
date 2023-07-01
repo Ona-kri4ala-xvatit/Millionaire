55c22b1819d992d38ba06d10195b54b14fed27aa
Nijat Muradov <live4ever4@gmail.com>
2023-07-01 19:02:03 +0400

    Add model and first testing

---
+++ Millionaire/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Millionaire
+{
+    public class Data
+    {
+        public string? Question { get; set; }
+        public string[] Answer { get; set; } = new string[4];
+        public int? RightIndex { get; set; }
+    }
+}
--- Millionaire/MainWindow.xaml.cs
+++ Millionaire/MainWindow.xaml.cs
@@ -1,16 +1,31 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Shapes;
 
 namespace Millionaire
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        Data data = new Data();
 
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = data;
+
+            data.Question = "Which of the planets is closest to the sun?";
+            data.Answer[0] = "Venus";
+            data.Answer[1] = "Earth";
+            data.Answer[2] = "Mars";
+            data.Answer[3] = "Mercury";
+            data.RightIndex = 1;
+
         }
     }
 }
